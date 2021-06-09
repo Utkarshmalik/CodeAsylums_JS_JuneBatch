3407485577e85bd928196cf87ecddb76a7de103b
mohitverma6161 <utkarshmalik06@gmail.com>
2021-06-09 22:46:54 +0530

    JS basic syntax

---
+++ README.md
@@ -0,0 +1,1 @@
+# CodeAsylums_JS_JuneBatch
---
+++ script.js
@@ -0,0 +1,225 @@
+
+
+// // //Javascript is case Se
+// // //dynamic or static : dynamic
+
+
+
+
+
+
+// // //numbers,string,boolean,null,undefind,symbol, object 
+
+
+
+// // var counter=10;      //number 
+// // console.log(typeof counter);
+
+
+
+
+// // var myName;
+// // console.log(typeof myName);
+
+
+// // //null
+// // let someSentence=null;
+// // console.log(typeof someSentence);
+
+
+// // let someObj={} 
+
+// // let person={firstName:'Utkarsh',age:25};
+
+// // let a=10;
+// // let b="10";
+
+// // // == and ===
+// // // console.log(a==b);
+// // // console.log(a===b)
+
+// // //falsy values  : 0 , "", null , undefined , [] 
+// // //truth values   
+
+// // console.log(null===undefined);
+
+// // // var,let and const
+
+
+// // let a=10;
+// // let b=a;
+
+// // b++;
+// // console.log(a);
+// // console.log(b);
+
+// // let a="hello";
+// // let b=a;
+
+// // b+=" world";
+
+// // console.log(a);
+// // console.log(b);
+
+// let person1={
+//     name:"Utkarsh",
+//     age:25
+// }
+
+// let person2=person1;
+
+// person2.age++;
+
+// console.log(person1.age);
+// console.log(person2.age);
+
+
+// let x=5;
+
+// if(x>7)
+// {
+//     console.log('x is greater than 7');
+// }
+// else
+// {
+    
+//     console.log('x is less than or equal 7');
+// }
+
+
+
+// if(x>7)
+// {
+//     console.log('x is greater than 7');
+// }
+
+// else if(x<7)
+// {
+
+// }
+// else
+// {
+    
+//     console.log('x is less than or equal 7');
+// }
+
+
+
+//ternary operator 
+
+
+// var allow;
+// var age=24;
+
+// if(age>18)
+// {
+//     allow=true;
+// }
+// else
+// {
+//     allow=false;
+// }
+
+//var vairble= conditon ? exp1 : exp2
+
+
+// var allow= (age>18)?true:((height>180)?true:false);
+
+
+
+
+//switch case statement 
+
+
+// var day=9 ;
+
+// var dayName;
+
+
+// switch(day)
+// {
+//     case 1:
+//         dayName='Sunday';
+//         break;
+    
+//     case 2:
+//         dayName='Monday';
+//         break;
+    
+//     case 3:
+//         dayName='Tuesday';
+//         break;
+    
+//         default:
+//         dayName='Invalid';
+// }
+
+// console.log(dayName);
+
+
+
+// let count=1;
+
+// while(count < 10)
+// {
+//     console.log(count);
+//     count+=2;
+// }
+
+
+// let count=0;
+// do
+// {
+//     count++;
+//     console.log(count);
+// }while(count < 10)
+
+
+
+// for(var i=1;i<5;i++)
+// {
+//     console.log(i);
+// }
+
+
+// for(var i=1;i<10;i++)
+// {
+//     if(i%3==0)
+//     {
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+
+
+// console.log(i);
+
+
+
+// console.log(undefined==null);
+// console.log(!undefined === !null);
+
+// console.log(!null);
+// console.log(null==false);
+// console.log(null===false);
+//  console.log(null==true)
+
+
+
+//null value 0 : maths operations
+console.log(null+5);
+console.log(null-5);
+console.log(null/5);
+console.log(null%5);
+
+
+
+
+console.log(null==false);
+console.log(undefined==false);
+
+
+
+console.log(!null === !undefined);
