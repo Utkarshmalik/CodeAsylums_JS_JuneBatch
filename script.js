e1e4d668d24c6ab2c502aecbe5d9281864cd5922
mohitverma6161 <utkarshmalik06@gmail.com>
2021-06-12 15:00:40 +0530

    arrays

--- script.js
+++ script.js
@@ -1,120 +1,147 @@
 
 
 
-// function  add() {
+//let arr=[1,2,3,4,5];
 
-//     let sum=0;
-
-//     for(let i=0;i<arguments.length;i++)
-//     {
-//         sum+=arguments[i];
-//     }
-
-//     return sum;
-// }
-
-// console.log(add(1));
-// console.log(add(1,2));
-// console.log(add(1,2,3));
-// console.log(add(1,2,3));
-// console.log(add(1,2,3,4));
-// console.log(add(1,2,4,5,6));
-
-
-//Functions in JS are first class citizens
-
-
-// let sum=function(a,b){
-
-//     console.log(a+b);
-// }
-
-// let add=sum;
-
-// add(3,4);
+// let arr=new Array(5);
+// let marks=new Array(10,11,12,13,14);
 
 
 
-// function sum(a,b){
+let marks=[10,20,30,40,60];
 
-//     return a+b;
-// }
+console.log(marks[1]);
+console.log(marks.length);
+
+// marks.push(70);
+// marks.pop();
 
 
-// function average(a,b,sum)
+// marks.unshift(5);
+// marks.shift();
+
+console.log(marks);
+
+
+// 1- forEach,2-map,3-filter,4-reduce,5-sort
+
+
+
+
+// let sum=0;
+
+// persons.forEach((person,index)=>
 // {
-//     return sum(a,b)/2;
-// }
+//     sum+=person.age;
+//     // console.log(`${person.name} is ${person.age} years old`);   
+// });
 
-// let add=sum;
-
-// console.log(average(10,30,add));
-
-
-// //average of two number : sum/2
+// console.log(sum);
 
 
+// useCases : array->transform->newArray
 
-// function outer()
+
+// const arr=[1,2,3,4,5];
+
+
+// const res=arr.map((element,index)=>
 // {
-//     let x=10;
+//     return element *5;
+// })
 
-//     return function inner()
-//     {
-//         let y=20;
+// console.log(arr);
+// console.log(res);
 
-//         return x+y;
-//     }
-// }
+// const res=persons.map((person,index)=>
+// {
+//     //clone a JS 
 
-// let y=outer();
-// console.log(y());
+//     //spread operator 
 
+//     const newObj={...person};
+//     newObj.salary+=100;
+//     return newObj;
+// })
 
-
-//anonymous functions
-
-// let fun=function() {
-//     console.log("fun");
-// }
-
-// setTimeout(function() {
-//     console.log("hello")  
-// },3000);
+// console.log(persons);
+// console.log(res);
 
 
-//IIFE : immidiately invoked function exectuion
+// let persons=[{name:"Utkarsh",age:25,salary:1000,cgpa:9.6},{name:"Rahul",age:20,salary:31000,cgpa:5.5},{name:"Shreya",age:30,salary:6000,cgpa:6.78},{name:"Shiv",age:22,salary:7000,cgpa:8.9}];
 
-(function (){
-    console.log("IIFE");
-})();
+// //>=8
+
+// //filter things from an array 
+
+// const eligiblePersons=persons.filter((person,index)=>
+// {
+//     return person.cgpa>=8;
+// })
+
+// console.log(persons);
+// console.log(eligiblePersons);
 
 
-//ES6 : arrow functions
+//reduce method 
+//[1,3,5,6,,78,,6,,3,5,5]=>some Single single
+
+//array.reduce(reducer,[,initialValue]);
+//reducer function is called with two things accumulator and the current value 
+//reducer function returns a value which is the accumulated result 
+
+let arr=[13,5,4,2,4,5,33];
+
+//reduce an array to a single value : 
+
+     const res=arr.reduce((accumulator,currentElementValue)=>
+     {
+         return accumulator+currentElementValue;
+     },0);
+
+     console.log(res);
+     
+    // element    accumulator     accumatedResult 
+    //   13           0               13
+    //   5            13              18
+    //   4            18              22
+    //   2            22              24
+    //   4            24              28
+    //   5            28              33
+    //   33           33              66
 
 
-let fun=()=>
+
+    
+// arr.sort((a,b)=>
+// {
+//     if(a<b)
+//     return -1;
+
+//     if(a>b)
+//     return 1;
+
+//     return 0;
+
+// });
+
+// console.log(arr);
+
+
+
+let persons=[{name:"Utkarsh",age:25,salary:1000,cgpa:9.6},{name:"Rahul",age:20,salary:31000,cgpa:5.5},{name:"Shreya",age:30,salary:6000,cgpa:6.78},{name:"Shiv",age:22,salary:7000,cgpa:8.9}];
+
+
+persons.sort((person1,person2)=>
 {
-    console.log("hello");
-}
-fun();
+    if(person1.cgpa<person2.cgpa)
+    return 1;
 
-let add=(a,b)=>
-{
-    return a+b;
-}
+    if(person1.cgpa>person2.cgpa)
+    return -1;
 
+    return 0;
 
-console.log(add(2,3));
+})
 
-
-
-// //callback functions
-
-// setTimeout(() => {
-//    console.log("rfeefe"); 
-// }, 4000);
-
-
-
-
+console.log(persons);
