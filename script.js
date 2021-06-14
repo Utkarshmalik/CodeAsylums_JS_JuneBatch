49a6f108b4969c3b540329d71353011c28f36039
mohitverma6161 <utkarshmalik06@gmail.com>
2021-06-14 22:06:50 +0530

    constructor function

--- script.js
+++ script.js
@@ -14,25 +14,25 @@
 
 
 
+// let person={
 
+//     firstName:"Utkarsh",
+//     lastName:"Malik",
+//     dob:"06/06/1994",
+//     getFullName:function() {
+            
+//              return function()
+//              {
+//                return this.firstName + " "+" "+this.lastName;
+//              }
+//       }
+// }
 
-let person={
+// const res=person.getFullName();
+// console.log(res());
 
-    firstName:"Utkarsh",
-    lastName:"Malik",
-    dob:"06/06/1994",
-    getFullName:function() {
-        
-        
-            return person.firstName + " "+" "+person.lastName;
-        
-    }
-}
+//lexically : by looking the physical position 
 
-
-
-const res=person.getFullName();
-console.log(res);
 // console.log(someInnerFun());
 
 
@@ -51,3 +51,65 @@ console.log(res);
 // window.x;
 
 // window.sayHi
+
+
+
+//construction functions 
+
+
+
+
+//  let person1={
+
+//      firstName:"Utkarsh",
+//      lastName:"Malik",
+// }
+
+
+// let person2={
+
+//     firstName:"dv",
+//     lastName:"Malik",
+// }
+
+
+
+//functions vs CF : 
+//CF always start wth a capital Letter
+// CF are called only with a new Operator 
+
+
+//this inside of constructor functions points to the new instance created by that 
+//Blue print 
+
+function Person(firstName,lastName)
+{
+    this.firstName=firstName;
+    this.lastName=lastName;
+
+    this.getFullName=function () {
+       return this.firstName+" "+this.lastName;
+    }
+}
+
+
+//new operator 
+
+let person1=new Person('Utkarsh','Malik');
+let person2=new Person("Rahul","Jain");
+
+//1000 , 1000000000
+
+
+console.log(person1.getFullName());
+console.log(person2.getFullName());
+
+
+
+
+console.log(person1);
+console.log(person2);
+
+
+//Protypes 
+//prototypal inheritence 
