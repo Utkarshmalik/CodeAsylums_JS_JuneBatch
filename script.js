2b968a0b0ec3d81b26da7dd46e57172b3714922b
mohitverma6161 <utkarshmalik06@gmail.com>
2021-06-16 22:13:30 +0530

    prototypal inheritence

--- script.js
+++ script.js
@@ -1,115 +1,64 @@
 
 
 
+const arr=new Array(1,2,3);
 
-// const someKey="name";
-// console.log(person[someKey]);
-// //  person.name
+console.log(typeof Array);
 
-
-
-//this keyword 
-
-//inside the methods , this references the object used to invoke that method 
+console.log(typeof Object);
 
 
-
-// let person={
-
-//     firstName:"Utkarsh",
-//     lastName:"Malik",
-//     dob:"06/06/1994",
-//     getFullName:function() {
-            
-//              return function()
-//              {
-//                return this.firstName + " "+" "+this.lastName;
-//              }
-//       }
-// }
-
-// const res=person.getFullName();
-// console.log(res());
-
-//lexically : by looking the physical position 
-
-// console.log(someInnerFun());
-
-
-// console.log(person.getFullName());
+//Array , Object
 
 
 
 
-// var x=5;
-
-// function sayHi()
-// {
-//     console.log("hii");
-// }
-
-// window.x;
-
-// window.sayHi
-
-
-
-//construction functions 
-
-
-
-
-//  let person1={
-
-//      firstName:"Utkarsh",
-//      lastName:"Malik",
-// }
-
-
-// let person2={
-
-//     firstName:"dv",
-//     lastName:"Malik",
-// }
-
-
-
-//functions vs CF : 
-//CF always start wth a capital Letter
-// CF are called only with a new Operator 
-
-
-//this inside of constructor functions points to the new instance created by that 
-//Blue print 
-
-function Person(firstName,lastName)
-{
+function Person(firstName,lastName){
     this.firstName=firstName;
     this.lastName=lastName;
 
-    this.getFullName=function () {
-       return this.firstName+" "+this.lastName;
-    }
 }
 
 
-//new operator 
 
-let person1=new Person('Utkarsh','Malik');
-let person2=new Person("Rahul","Jain");
-
-//1000 , 1000000000
-
-
-console.log(person1.getFullName());
-console.log(person2.getFullName());
+Person.prototype.sayHi=function()
+{
+    console.log(`${this.firstName} is saying hii`)
+}
 
 
+const person1=new Person("Utkarsh",'de');
+const person2=new Person("Rahul",'jain');
 
 
-console.log(person1);
-console.log(person2);
+//[[prototype]]://__proto__
+
+// console.log(person1.__proto__===Person.prototype);
+// console.log(Person.prototype.__proto__===Object.prototype);
+// console.log(Object.prototype.__proto__);
 
 
-//Protypes 
-//prototypal inheritence 
+const arr1=new Array(1,2,4);
+
+console.log(arr1.toString());
+
+
+// //Array.prototype
+
+// console.log(arr1.__proto__===Array.prototype);
+
+// console.log(Array.prototype.__proto__===Object.prototype); 
+
+// console.log(arr1.__proto__.__proto__.__proto__);
+
+
+// // Everything is JS is an object 
+
+
+// const s1=new String("hello");
+// console.log(s1.__proto__.__proto__);
+
+
+
+
+
