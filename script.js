12acaba792bcb566d54e28472f5f1d859b360ae6
mohitverma6161 <utkarshmalik06@gmail.com>
2021-06-19 22:06:15 +0530

    JS runtime and hoisting

--- script.js
+++ script.js
@@ -1,80 +1,24 @@
-// // let car = {
-// //     brand: 'Honda',
-// //     getBrand: function () {
-// //         return this.brand;
-// //     }
-// // }
-
-// // console.log(car.getBrand());
 
 
-// // let brand=car.getBrand;
+// fun();
 
-// // brand();
+console.log(x);
+let x=5;
 
+// //undefined
 
-// //function methods :call , apply and bind 
-
-
-// let person1={
-//     name:"Utkarsh",
-//     money:100,
-//     depositMoney(x)
-//     {
-//         console.log(`${this.name} is depositing ${x} money in bank`);
-//     }
+// var fun=function() {
+//     const ole.log("hello");
 // }
 
-// let person2={
-//     name:"Rahul",
-//     money:1000
-// }
-
-// person2.depositMoney=person1.depositMoney.bind(person2);
-// person2.depositMoney(60);
+// //var : 
+// //let and const 
 
 
 
-// // const RahulsDepositMoneyFunction=person1.depositMoney.bind(person2);
-// // person2.depositMoney=RahulsDepositMoneyFunction;
-
-// // person2.depositMoney(10);
 
 
+//Hoisting
 
-// function sayHi(greeting,Question)
-// {
-//     console.log(`${this.name} is saying ${greeting} and asking ${Question}`);
-// }
-
-// //i want to explicitly tell the method what should be the value of "this" in that method
-
-// //call
-
-// // sayHi("hello","how are you ?");
-// // sayHi.call(person1,"hello","how are you ?");
-// // sayHi.call(person2,"hello","how are you ?");
-
-
-// //apply
-// sayHi.apply(person2,["hello","how are you ?"]);
-
-
-// //bind
-
-// const newFun=sayHi.bind(person1,"hello","how are you ?");
-// newFun();
-
-
-
-function Car() {
-    this.speed = 120;
-}
-
-Car.prototype.getSpeed = () => {
-    return this.speed;
-}
-
-var car = new Car();
-console.log(car.getSpeed());
-
+// var's are always pRTIALLY HOISTED
+// functions are fully hoisted 
