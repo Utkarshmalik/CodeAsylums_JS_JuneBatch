b8737a1dcd0b0a7a371ba86ea59470c2ddeea297
mohitverma6161 <utkarshmalik06@gmail.com>
2021-06-20 22:08:45 +0530

    asychronous JS

--- script.js
+++ script.js
@@ -1,24 +1,87 @@
 
 
-// fun();
-
-console.log(x);
-let x=5;
-
-// //undefined
-
-// var fun=function() {
-//     const ole.log("hello");
-// }
-
-// //var : 
-// //let and const 
 
 
 
 
+//call to fetch the data from a third party
 
-//Hoisting
 
-// var's are always pRTIALLY HOISTED
-// functions are fully hoisted 
+
+// //APi call using fetch in JS
+
+
+//Side effect
+// fetch("https://jsonplaceholder.typicode.com/users");
+
+//to handle synchrouns tasks in JS -> callbacks , promises , asycn await 
+
+//purpose is to help writing synchrounous piece of code in JS
+
+// //side effect
+// setTimeout(() => {
+//     console.log("hello");
+// }, 4000);
+
+// console.log("Hii");
+
+
+
+
+
+// //fetch(url) => Promise
+
+
+// //promise
+
+
+
+
+// let someWork=new Promise((resolve,reject)=>
+// {
+
+//     //here i will perform the side effect 
+
+//     setTimeout(() => {
+    
+//         let success=true;
+
+//         if(success)
+//         resolve("data");
+//         else
+//         reject("some eror");
+
+//     }, 4000);
+
+    
+// // })
+
+// someWork.then((value)=>
+// {
+//     console.log(value);
+// })
+// .catch((err)=>
+// {
+//     console.log(err);
+// })
+// .finally(()=>
+// {
+//     console.log("promised settled");
+// })
+
+// //promise chanining 
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((reponse)=>
+{
+     return reponse.json();
+})
+.then((users)=>
+{
+    console.log(users);
+})
+.catch((err)=>
+{
+    console.log(err);
+})
