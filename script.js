13564319133b7c0e32d9d988a78b0b48d162773d
mohitverma6161 <utkarshmalik06@gmail.com>
2021-06-26 20:02:56 +0530

    basic todo list day 1

--- script.js
+++ script.js
@@ -1,46 +1,101 @@
 
-
-// const root=document.getElementById('root');
-
-// //h1
-
-// //createElement
-// const newHeading=document.createElement('h1');
-
-// //textNode to a head 
-// newHeading.textContent="i am a new heading";
+const submitButton=document.getElementById('submit-button');
+const incompleteSection=document.getElementById('incomplete-section').firstElementChild.nextElementSibling;
+const completedSection=document.getElementById('complete-section').firstElementChild.nextElementSibling;
 
 
 
-// //innerHTML
-// root.appendChild(newHeading)
-
-const todoList=['sleep','dance','code','drink','sing'];
+let incompletedItems=[];
+let completedItems=[];
 
 
- const root=document.getElementById('root');
+(function initialLoad() {
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+    .then(res=>res.json())
+    .then(data=>{
+        //spread operator
+        incompletedItems=[...data];
+        renderIncompleteList();
+    })
+})()
 
- //ul TYPE
- const list=document.createElement('ul');
+function prepareListItem(newTask)
+{
+    const newLi=document.createElement('li');
+    const parentDiv=document.createElement('div');
+    const childDiv=document.createElement('span');
+    const text=document.createElement('p');
+    text.innerText=newTask.title;
+    const btn1=document.createElement("button");
+    btn1.innerText="Done";
+    const btn2=document.createElement("button");
+    btn2.innerText="Delete";
+    newLi.setAttribute("id",newTask.id);
+    parentDiv.appendChild(text);
+    childDiv.appendChild(btn1);
+    childDiv.appendChild(btn2);
+    parentDiv.appendChild(childDiv);
+    newLi.appendChild(parentDiv);
 
- list.setAttribute("id","list");
+    return newLi;
 
- todoList.forEach((item)=>
- {
-     const newItem=document.createElement('li');
-     newItem.textContent=item;
-     newItem.setAttribute("class","item");
-     list.appendChild(newItem);
+}
 
-    //  newItem.style.color='red';
-    //  newItem.style.backgroundColor='blue';
-    //  newItem.style.fontSize='30px';
+function renderIncompleteList()
+{
+    incompletedItems.forEach((newTask)=>
+    {
+      
+        const listItem=prepareListItem(newTask);
+        incompleteSection.appendChild(listItem);
+    })
+}
 
-    newItem.setAttribute("style",'color:yellow;background-color:blue;font-size:30px')
+function renderIncompleteListItem(newTask)
+{
+      const listItem=prepareListItem(newTask);
+        incompleteSection.appendChild(listItem);
+}
 
- })
-
- root.appendChild(list);
 
 
+// function  renderCompleteList() {
+    
+//     completedItems.forEach((newTask)=>
+//     {
+//         const newLi=document.createElement('li');
+//         newLi.innerText=newTask.title;
+//         newLi.setAttribute("id",newTask.id);
+//         completedSection.appendChild(newLi);
+//     })
+// }
+
+
+const createNewListItem=(value)=>
+{
+    const randID=Math.floor((Math.random() * 10000000) + 1);
+
+    const newTask={
+        "id": randID,
+        "title": value,
+        "completed": false
+    }
+
+    return newTask;
+}
+
+
+//any event hadler 
+
+const onButtonClick=()=>
+{
+    const inputField=document.getElementsByName('task')[0];
+
+    //new List item in incompleted section
+    const task=createNewListItem(inputField.value);
+    incompletedItems.push(task);
+    renderIncompleteListItem(task);
+}
+
+submitButton.addEventListener("click",onButtonClick);
 
