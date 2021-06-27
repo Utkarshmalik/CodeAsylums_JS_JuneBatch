e5785ce9fa626aae53aa996122418ed528c7112f
mohitverma6161 <utkarshmalik06@gmail.com>
2021-06-27 18:31:12 +0530

    basic project done

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const incompleteSection=document.getElementById('incomplete-section').firstEleme
 const completedSection=document.getElementById('complete-section').firstElementChild.nextElementSibling;
 
 
-
 let incompletedItems=[];
 let completedItems=[];
 
@@ -14,10 +13,78 @@ let completedItems=[];
     .then(res=>res.json())
     .then(data=>{
         //spread operator
-        incompletedItems=[...data];
+        incompletedItems=data.filter((task)=>
+        {
+            return task.completed===false;
+        });
+        completedItems=data.filter((task)=>
+        {
+            return task.completed===true;
+        })
+    
         renderIncompleteList();
+        renderCompleteList();
+        
     })
 })()
+
+//in all the event handlers 'this' points to the element who tirggered that event  
+
+function onDoneButtonClick(e) {
+    
+    const clickedElement=this.parentElement.parentElement.parentElement;
+    const clickedId=clickedElement.id;
+
+
+
+   const clickedTask=incompletedItems.find(task=>task.id==clickedId);
+    console.log(clickedTask);
+
+    incompletedItems=incompletedItems.filter((task)=>
+    {
+        return task.id!==clickedId;
+    })
+
+    clickedTask.completed=true;
+
+    completedItems.push(clickedTask);
+    clickedElement.firstElementChild.lastElementChild.removeChild(clickedElement.firstElementChild.lastElementChild.firstElementChild);
+    completedSection.appendChild(clickedElement);
+    incompleteSection.removeChild(clickedElement);
+
+}
+
+function onDeleteButtonClick() {
+
+     
+    const clickedElement=this.parentElement.parentElement.parentElement;
+    const clickedId=clickedElement.id;
+
+    incompletedItems=incompletedItems.filter(task=>task.id!==clickedId);
+    completedItems=completedItems.filter(task=>task.id!==clickedId);
+
+
+    console.log(clickedElement);
+    try
+    {
+    incompleteSection.removeChild(clickedElement);
+    }
+    catch(e)
+    {
+        
+    }
+
+    try
+    {
+    completedSection.removeChild(clickedElement);
+    }
+    catch(e)
+    {
+        
+    }
+
+
+}
 
 function prepareListItem(newTask)
 {
@@ -31,25 +98,41 @@ function prepareListItem(newTask)
     const btn2=document.createElement("button");
     btn2.innerText="Delete";
     newLi.setAttribute("id",newTask.id);
+
+    btn1.addEventListener("click",onDoneButtonClick);
+    btn2.addEventListener("click",onDeleteButtonClick);
+
     parentDiv.appendChild(text);
+
+    if(!newTask.completed)
     childDiv.appendChild(btn1);
+
     childDiv.appendChild(btn2);
     parentDiv.appendChild(childDiv);
     newLi.appendChild(parentDiv);
 
     return newLi;
-
 }
 
 function renderIncompleteList()
 {
     incompletedItems.forEach((newTask)=>
     {
-      
         const listItem=prepareListItem(newTask);
         incompleteSection.appendChild(listItem);
     })
 }
+
+
+function  renderCompleteList() {
+    
+    completedItems.forEach((newTask)=>
+    {
+        const listItem=prepareListItem(newTask);
+        completedSection.appendChild(listItem);
+    })
+}
+
 
 function renderIncompleteListItem(newTask)
 {
@@ -74,13 +157,11 @@ function renderIncompleteListItem(newTask)
 const createNewListItem=(value)=>
 {
     const randID=Math.floor((Math.random() * 10000000) + 1);
-
     const newTask={
         "id": randID,
         "title": value,
         "completed": false
     }
-
     return newTask;
 }
 
